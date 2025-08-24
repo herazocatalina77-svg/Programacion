60980adcc87add6584bcf45170957473c9b3af48
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-08-24 16:46:30 -0500

    Add files via upload

---
+++ Clasequincedeagosto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+	static void Main(string[] args)
+	{
+		int num1 = 0;
+		int num2 = 0;
+		int num3 = 0;
+
+		Console.WriteLine("Ingresa el número 1:");
+		num1=Convert.ToInt32(Console.ReadLine());
+	}
+}
