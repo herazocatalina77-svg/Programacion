11ad0d65dd89a8f423d0fc5b82c1b5e66540711c
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-08-29 08:56:32 -0500

    Add files via upload

---
+++ Controladores y acumuladores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+    namespace tikiti
+{
+    internal class Repe
+    {
+        static void Main (string[] args)
+        {
+            int contador = 0;
+            int acumulador = 0;
+            while (contador <= 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"Contador: {contador} - acumulador {acumulador}");
+                contador++;
+            }
+            Console.WriteLine ("La suma de los primeros cinco numeros enteros  es" + acumulador);
+        }
+    }
+}
