4324c61b010ea9c6e671f21ce8ffbb890e976815
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-09-03 12:29:54 -0500

    Add files via upload

---
+++ Adivinarelnumero3sep.cs
@@ -0,0 +1,27 @@
+﻿using System;
+    internal class Septiembre3
+{
+    static void Main(string[] args)
+    {
+        int numero;
+        Random rnd = new Random();
+        numero = rnd.Next(2, 101);//Devuelve un número entre 1 y 100
+        int numerousuario = 0;
+        Console.WriteLine("Ingrese un numero del 1 al 100");
+        numerousuario =Int32.Parse(Console.ReadLine());
+        while ( numerousuario != numero)
+        {
+            if ( numerousuario > numero )
+            {
+                Console.WriteLine("El numero que escogiste es muy grande");
+            }
+            else
+            {
+                Console.WriteLine("El numero que escogiste es muy pequeno");
+            }
+            Console.WriteLine("Intenta otra vez");
+            numerousuario = Int32.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Adivinaste el numero");
+    }
+}
