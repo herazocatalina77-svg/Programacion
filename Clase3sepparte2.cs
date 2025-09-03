faebda2eee1f4f81c8172c6540fe304cd99d39e8
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-09-03 09:00:08 -0500

    Add files via upload

---
+++ Clase3sepparte2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace _13.while5
+{
+    internal class Clse3sepparte2
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            Console.WriteLine("Ingrese numeros aleatorios");
+            while (contador <=8)
+            {
+                Console.WriteLine($"Su cantidad de numeros son {contador}");
+            }
+            Console.WriteLine($"Su cantidad de numeros son {contador}");
+        }
+    }
+}
