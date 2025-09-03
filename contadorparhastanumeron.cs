66e2f9a6fbc828c4f06ae4d6b40f0a5d90e0f5d2
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-09-03 09:19:33 -0500

    Add files via upload

---
+++ contadorparhastanumeron.cs
@@ -0,0 +1,22 @@
+﻿using System;
+internal class Sep3clase3
+{
+    static void Main(string[] args)
+    {
+        // Algoritmo que cuente cuantos numeros pares hay en un rango del 1 al numero n
+        int rango = 0;
+        int contadorPares = 0;
+        int contador = 1;
+        Console.WriteLine("Ingrese el rango a evaluar");
+        rango=int.Parse(Console.ReadLine());
+        while (contador<=rango)
+        {
+            if (contador % 2 == 0)
+            {
+                contadorPares++;
+            }
+            contador++;
+        }
+        Console.WriteLine($"La cantidad de numeros pares de 1 hasta {rango} es: {contadorPares}");
+    }
+}
