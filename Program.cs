dfc39e8448ddca1bf9620d6bb5c0a4584ac687c0
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-09-17 09:36:59 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,43 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-public class Program
+﻿using System;
+namespace _23.Vectores
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        string nombre = "";
-        int sueldo = 0;
-        Console.WriteLine("Ingrese el nombre");
-        nombre = Console.ReadLine();
-        Console.WriteLine("Ingrese el sueldo");
-        sueldo = Int32.Parse(Console.ReadLine());
-        if (sueldo < 3000)
+        static void Main(string[] args)
         {
-            //Verdadero
-            Console.WriteLine($"La persona (nombre), debe abonar impuestos");
+            {
+                //Arreglos Unidimensionales
+                //Vectores
+                int[] numeros = new int[5]; //Elvectornumero tiene 5 posiciones
+                string[] nombres = new string[4];
+                //Asignar valores a las posiciones
+                numeros[0] = 20;
+                numeros[1] = 15;
+                numeros[2] = 5;
+                numeros[3] = 30;
+                numeros[4] = 45;
+                //vectores inicializados por defecto
+                int[] numeros1 = new int[] { 1, 2, 3, 4, 5 };
+                int[] numeros2 = { 5, 12, 4, 7, 9 };
+                char[] caracter = new char[4];
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Ingrese el caracter para la posicion {i + 1}, con indice {i}:");
+                    caracter[i]=Convert.ToChar(Console.ReadLine());
+                }
+                Console.WriteLine($"El vector caracter, en la posición 3, tiene almacenado el dato{caracter[2]}");
+                for ( int i = 0; i < caracter.Length; i++)
+                {
+                    Console.WriteLine("\n" + caracter[i] + "|");
+                }
+                for (int i = 'A'; i < 5;)
+                {
+
+                }
+
+            }
         }
-        else
-        {
-            //False
-            Console.WriteLine($"La persona (nombre), no abona impuestos");
-        }
-    }
-    static void Second(string[] args)
-    {
-        int edad = 0;
-        Console.WriteLine("Ingrese edad");
-        if (edad < 18)
-        {
-            //Verdadero
-            Console.WriteLine("Bienvenido al sitio web");
-        }
-        else
-        {
-            //falso
-            Console.WriteLine("No es apto para este sitio web");
-        }
-    } 
-    static void Third(string[] args)
-    {
-        int num = 0;
     }
 }
